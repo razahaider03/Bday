a69fcaf15756c4cb284d57ee343e0deff50e3368
Raza Haider <razahaider03@gmail.com>
2020-08-15 10:41:36 +0530

    added negate things in gitignore

--- app/src/main/java/random/calculate.java
+++ app/src/main/java/random/calculate.java
@@ -7,11 +7,12 @@ public class calculate {
 
   public static void main(String[] args) {
     System.out.println(day(20));
+    System.out.println("hello everyone");
   }
 
   public static List<Integer> day(int size) {
     Random rand = new Random();
-    List<Integer> name = new ArrayList<Integer>(size);
+    List<Integer> name =new ArrayList<>(size);
     int count = 0;
     do {
       int days = rand.nextInt(365);
