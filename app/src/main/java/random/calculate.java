ec7e0615afc6cea1633a78f9d45454afd2014913
Raza Haider <razahaider03@gmail.com>
2020-08-16 08:45:00 +0530

    completed Assignments birthday problem

--- app/src/main/java/random/calculate.java
+++ app/src/main/java/random/calculate.java
@@ -3,22 +3,41 @@ package random;
 import java.util.*;
 import java.util.Random;
 
+
+
 public class calculate {
 
-  public static void main(String[] args) {
-    System.out.println(day(20));
-    System.out.println("hello everyone");
-  }
+    public static
+    void main ( String[] args ) {
+        System.out.println("hello everyone");
+        //System.out.println(day(28));
+        int count=2000,duplicate=0;
+        for ( int i=0; i<count; i++ ) {
+            // day(28,i+1);
+            if (day(20,i+1)==1){
+                duplicate++;
+            }
+        }
+            System.out.println((double) duplicate/count*100);
+    }
+        private static int day(int size, int seed){
+            Random rand=new Random();
+            rand.setSeed(seed);
+            List<Integer> Bday=new ArrayList<>(size);
+            // List<Integer> rep = new ArrayList<>(10); // for check which are same.
+            int loop=0, same=0;
+            do {
+                int days=rand.nextInt(365);
 
-  public static List<Integer> day(int size) {
-    Random rand = new Random();
-    List<Integer> name =new ArrayList<>(size);
-    int count = 0;
-    do {
-      int days = rand.nextInt(365);
-      name.add(days);
-      count++;
-    } while (count != size);
-    return name;
-  }
+                if (!Bday.contains(days)) {
+                    Bday.add(days);
+                } else if (Bday.contains(days)) {
+                    same++;
+                    break;
+                }
+                loop++;
+            } while (loop != size);
+            return same;
+        }
+
 }
